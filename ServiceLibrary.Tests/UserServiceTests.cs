744f60603a6e62481259d8bb7b1f086a35839939
atimormia <atimormia@gmail.com>
2017-02-14 23:29:43 +0300

    first day task

---
+++ ServiceLibrary.Tests/UserServiceTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceLibrary.Tests
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Add_NullUser_ExceptionThrown()
+        {
+            UserService service = new UserService();
+            service.Add(null);
+        }
+
+        [TestMethod]
+        public void Add_EmptyUser_Success()
+        {
+            UserService service = new UserService();
+            User user = new User();
+            service.Add(user);
+        }
+
+        [TestMethod]
+        public void Add_DefaultUser_Success()
+        {
+            UserService service = new UserService();
+            User user = new User
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                DateOfBirth = new DateTime(2000, 9, 9)
+            };
+            service.Add(user);
+        }
+
+        [TestMethod]
+        public void Add_DoubleUser_Success()
+        {
+            UserService service = new UserService();
+            User user = new User
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                DateOfBirth = new DateTime(2000, 9, 9)
+            };
+            service.Add(user);
+            service.Add(user);
+        }
+
+        [TestMethod]
+        public void Delete_UnexistedUser_Success()
+        {
+            UserService service = new UserService();
+            User user = new User
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                DateOfBirth = new DateTime(2000, 9, 9)
+            };
+            service.Delete(user);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Delete_NullUser_Exception()
+        {
+            UserService service = new UserService();
+            service.Delete(null);
+        }
+
+        [TestMethod]
+        public void Delete_ExistedUser_Success()
+        {
+            UserService service = new UserService();
+            User user = new User
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                DateOfBirth = new DateTime(2000, 9, 9)
+            };
+            service.Add(user);
+            service.Delete(user);
+        }
+
+        [TestMethod]
+        public void Search_UnexistedUser_Null()
+        {
+            UserService service = new UserService();
+            var searchResult = service.Search(x => x.FirstName == "FirstName");
+            Assert.IsNull(searchResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Search_Null_Exception()
+        {
+            UserService service = new UserService();
+            service.Search(null);
+        }
+
+        [TestMethod]
+        public void Search_ExistedUser_ListOfUsers()
+        {
+            UserService service = new UserService();
+            User user = new User
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                DateOfBirth = new DateTime(2000, 9, 9)
+            };
+            service.Add(user);
+            var searchResult = service.Search(x => x.FirstName == "FirstName");
+            Assert.IsNotNull(searchResult);
+        }
+    }
+}
---
+++ ServiceLibrary/UserService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ServiceLibrary
+{
+    public class UserService
+    {
+        struct UserEntity
+        {
+            internal int Id { get; }
+            internal User User { get; }
+            public UserEntity(int id, User user)
+            {
+                Id = id;
+                User = user;
+            }
+        }
+        private readonly List<UserEntity> userEntities = new List<UserEntity>();
+
+        public void Add(User user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            int newId = 1; // TODO: generate new id!
+            userEntities.Add(new UserEntity(newId, user));
+            //return newId;
+        }
+
+        public void Delete(User user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            userEntities.RemoveAll(x => x.User.Equals(user));
+        }
+
+        public IEnumerable<User> Search(params Predicate<User>[] predicates)
+        {
+            if (predicates == null) throw new ArgumentNullException(nameof(predicates));
+
+            if (!userEntities.Any())
+                return null;
+            if (!predicates.Any())
+                return userEntities.Select(userEntity => userEntity.User);
+            var commonPredicate = predicates[0];
+            if (predicates.Length > 1)
+                foreach (var predicate in predicates)
+                    commonPredicate += predicate;
+            var foundEntities = userEntities.Where(x => commonPredicate(x.User));
+            var entities = foundEntities as IList<UserEntity> ?? foundEntities.ToList();
+            return entities.Any() ? entities.Select(userEntity=>userEntity.User) : null;
+        }
+
+    }
+}
